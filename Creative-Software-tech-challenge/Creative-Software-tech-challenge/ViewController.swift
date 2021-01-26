fd722370218415a52d62ca9a89a16824ca060eef
Kyeore Heo <91kyoheo@gmail.com>
2021-01-26 15:59:17 -0500

    first commit

---
+++ Creative-Software-tech-challenge/Creative-Software-tech-challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Creative-Software-tech-challenge
+//
+//  Created by Kyo on 1/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
